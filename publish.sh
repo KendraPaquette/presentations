6c9dfb047c70f123d4c6618bf9e80c4adcde6ab2
Kendra Wyant <kpaquette2@wisc.edu>
2025-03-28 19:56:56 -0500

    workflow updates

--- publish.sh
+++ publish.sh
@@ -1,3 +1,14 @@
-quarto publish gh-pages
-rm -r _manuscript
-rm -r index_files
+#!/bin/bash  
+
+# ./publish.sh FILE
+# FILE = filename
+
+FILE=$1
+
+echo ""
+echo "Publishing $FILE to quarto-pub"
+echo ""
+quarto publish quarto-pub "$FILE" --no-browser
+rm _quarto.yml
+rm -r *_files
+rm *.html
